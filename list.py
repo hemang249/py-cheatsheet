d19acce3dac6c1ace88ff4273a35342c60ea3908
Hemang Bhogayata <hemang249@gmail.com>
2019-07-21 22:43:17 +0530

    Added Lists Cheatsheet

--- list.py
+++ list.py
@@ -1,5 +1,48 @@
 # Lists and Common List Operations In Python
 
 #List Declaration
+fruits = ['Apple', 'Orange', 'Strawberry', 'Mango']
+print(fruits)
 
-list1 = ['Apple', 'Orange', 'Strawberry', 'Mango']
+# Access Individual List Element
+    # list[index]
+print(fruits[0])
+print(fruits[-2])
+
+# Add Elements to the List
+    # append(value)
+fruits.append('Pineapple')
+print(fruits)
+
+# Insert Element at a particular index
+    # insert(index , value)
+fruits.insert(0 , 'Banana')
+print(fruits)
+
+# Delete Element at a particular index
+    # del list[index]
+del fruits[0]
+print(fruits)
+
+# Pop Elements from List
+    # pop(index)
+fruits.pop(0)
+print(fruits)
+
+# Remove all occurences of an Value
+    # remove(value)
+fruits.remove('Mango')
+print(fruits)
+
+# Sorting a List
+    # sort()
+numbers = [7,6,51,64,123,84]
+numbers.sort()
+print(numbers)
+numbers.sort(reverse = True)
+print(numbers)
+
+# Find List Length
+    #len(list)
+print(len(numbers))
+

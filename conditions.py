998fae47f5e683b2d9396ac47e68d10b5c63ae0f
Hemang Bhogayata <hemang249@gmail.com>
2019-07-23 11:22:33 +0530

    Added Conditional Statements

---
+++ conditions.py
@@ -0,0 +1,35 @@
+# Common Conditional Statements used in Python 3
+
+# Basic if-else Condition
+x = 5
+if x == 5:
+    print("x = 5")
+else:
+    print("x != 5")
+
+
+# Basic Logical operations
+    # and or not
+a = 0
+b = 1
+boolean = False
+
+if a == 0 and b == 1:
+    print("a = 0 and b = 1")
+
+if a == 0 or b == 1:
+    print("Either a = 0 or b = 1")
+
+if not boolean :
+    print(boolean)
+
+# elif statements
+x = 1
+
+if x == 1:
+    print("x = 1")
+elif x == 2:
+    print("x = 2")
+elif x == 3:
+    print("x = 3")
+

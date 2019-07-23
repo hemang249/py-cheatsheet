753e77b208fba9e86df3a71e98226a0347281219
Hemang Bhogayata <hemang249@gmail.com>
2019-07-23 18:22:37 +0530

    Added Classes cheatsheet

---
+++ classes.py
@@ -0,0 +1,34 @@
+# Classes and basic OOP principles In Python3
+
+# Creating Classes and their instances
+class Pet():
+    
+    def __init__ (self ,name , breed , age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+        print('I have a ' + self.breed + ', his name is ' + self.name + ' and He is ' + self.age +' years old')
+
+    def food( self , foodtype):
+        print(self.name + " eats " + foodtype)
+
+my_pet = Pet('Cooper', 'Labrador', '1')
+my_pet.food('Pedigree')
+
+# Settig default attribute values
+class User():
+
+    def __init__(self , name , email , age):
+        self.name = name
+        self.email = email
+        self.age = age
+        self.date_of_birth = "NULL"
+
+    def print_info(self):
+        print("Name : " + self.name)
+        print("Email : " + self.email)
+        print("Age : " + str(self.age))
+        print("Date of Birth : " + self.date_of_birth)        
+
+myUser = User('Hemang', 'hemang@example.com', 18)
+myUser.print_info()

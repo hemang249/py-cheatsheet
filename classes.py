0f1e74917c32eef5ff3b1ee677b9980798487845
Hemang Bhogayata <hemang249@gmail.com>
2019-07-24 22:55:29 +0530

    Added Classes cheatsheet

--- classes.py
+++ classes.py
@@ -32,3 +32,22 @@ class User():
 
 myUser = User('Hemang', 'hemang@example.com', 18)
 myUser.print_info()
+
+# Inheritance in Pyhthon 3
+
+class Animals():
+
+    def __init__(self , species , name):
+        self.species = species
+        self.name = name
+    
+
+class Dog(Animals):
+
+    def __init__(self , species , name):
+        super().__init__(species , name)
+
+    def print_info():
+        print("Species : " + self.species + " Name : " + self.name)
+
+my_Dog = Dog("Labrador" , "Cooper")
